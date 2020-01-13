9d9eb3bdff4f739852fab659f1de6740547abed3
blake-m <blazej.marciniak.dent@gmail.com>
2020-01-13 09:37:51 +0100

    Continue Refactoring of Singly Linked List in Python

--- PythonCode/singly_linked_list.py
+++ PythonCode/singly_linked_list.py
@@ -1,20 +1,21 @@
 from typing import Any, Optional
 
+NodeOrNone = Optional["SinglyLinkedList.Node"]
 
 class SinglyLinkedList:
-    def __init__(self, first_element: Optional["SinglyLinkedList.Node"] = None):
+    def __init__(self, first_element: NodeOrNone = None):
         self.__size: int = 0
-        self.__head: None
-        self.__tail: None
+        self.__head: NodeOrNone = None
+        self.__tail: NodeOrNone = None
 
     class Node:
         def __init__(
                 self,
                 data: Optional[Any] = None,
-                next_: Optional["SinglyLinkedList.Node"] = None
+                next_: NodeOrNone = None
         ) -> None:
-            self.data: Any = data
-            self.next: "SinglyLinkedList.Node" = next_
+            self.data = data
+            self.next = next_
 
         def __str__(self):
             return str(self.data)
@@ -23,39 +24,65 @@ class SinglyLinkedList:
     def size(self) -> int:
         return self.__size
 
+    def ___len__(self) -> int:
+        return self.size
+
     def is_empty(self) -> bool:
         return self.__size == 0
 
-    def add_first(self, data: Any):
-        if self.is_empty():
-            node_to_add = SinglyLinkedList.Node(data)
-            self.__head = node_to_add
-            self.__tail = node_to_add
-        else:
-            pass
-        self.__size += 1
-        pass
+    def __add_to_empty_list(self, node: "SinglyLinkedList.Node"):
+        self.__head = node
+        self.__tail = node
 
-    def add_last(self):
-        pass
+    def add_first(self, data: Any):
+        node_to_add = SinglyLinkedList.Node(data)
+        if self.is_empty():
+            self.__add_to_empty_list(node_to_add)
+        else:
+            node_to_add.next = self.__head
+            self.__head = node_to_add
+        self.__size += 1
+
+    def add_last(self, data: Optional[Any]):
+        node_to_add = SinglyLinkedList.Node(data)
+        if self.is_empty():
+            self.__add_to_empty_list(node_to_add)
+        else:
+            self.__tail.next = node_to_add
+            self.__tail = node_to_add
+        self.__size += 1
 
     def clear(self):
-        pass
+        if not self.is_empty():
+            while self.__head.next is not None:
+                self.__head, self.__head = None, self.__head.next
+            self.__head = None
+            self.__tail = None
+            self.__size = 0
 
-    def __check_if_index_is_correct(self):
-        pass
+    def __check_if_index_is_correct(self, index: int):
+        if self.size == 0 and index == 0:
+            raise IndexError("The list is empty")
+        elif index < 0:
+            raise IndexError("Index can't be lower than 0.")
+        elif index >= self.size:
+            raise IndexError("Index out of range.")
 
-    def __get_node_at_index(self):
-        pass
+    def __getitem__(self, index: int):
+        self.__check_if_index_is_correct(index)
+        current_node = self.__head
+        for i in range(index):
+            current_node = current_node.next
+        return current_node.data
 
     def check_first(self):
-        pass
+        return self.__head
 
     def check_last(self):
-        pass
+        return self.__head
 
-    def check_at_index(self):
-        pass
+    def check_at_index(self, index: int):
+        return self[index]
 
     def remove_first(self):
         pass
--- PythonCode/tests/test_singly_linked_list.py
+++ PythonCode/tests/test_singly_linked_list.py
@@ -4,7 +4,7 @@ from singly_linked_list import SinglyLinkedList
 
 
 class TestSinglyLinkedListNode(unittest.TestCase):
-    def setUp(self) -> None:
+    def setUp(self):
         self.node = SinglyLinkedList.Node('Some Data')
         self.another_node = SinglyLinkedList.Node('Some Data', self.node)
 
@@ -15,8 +15,8 @@ class TestSinglyLinkedListNode(unittest.TestCase):
         self.assertEqual(self.another_node.data, self.another_node.next.data)
 
 
-class TestSinglyLinkedList(unittest.TestCase):
-    def setUp(self) -> None:
+class TestSinglyLinkedListEmpty(unittest.TestCase):
+    def setUp(self):
         self.sll = SinglyLinkedList()
 
     def test_size(self):
@@ -29,12 +29,79 @@ class TestSinglyLinkedList(unittest.TestCase):
     def test_is_empty(self):
         self.assertTrue(self.sll.size == 0)
 
-    def add_first(self):
-        self.sll.add_first()
-        pass
+    def test_add_first(self):
+        self.sll.add_first("Some data")
+        self.assertEqual(self.sll._SinglyLinkedList__head.data, "Some data")
+
+    def test_add_first_tail_head_are_same(self):
+        self.sll.add_first("Some data")
+        self.assertEqual(self.sll._SinglyLinkedList__head, self.sll._SinglyLinkedList__tail)
+
+    def test_add_first_size_increases(self):
+        self.sll.add_first("Some data")
+        self.assertEqual(self.sll.size, 1)
+
+    def test_add_last(self):
+        self.sll.add_last("Some data")
+        self.assertEqual(self.sll._SinglyLinkedList__head.data, "Some data")
+
+    def test_add_last_tail_head_are_same(self):
+        self.sll.add_last("Some data")
+        self.assertEqual(self.sll._SinglyLinkedList__head, self.sll._SinglyLinkedList__tail)
+
+    def test_add_last_size_increases(self):
+        self.sll.add_last("Some data")
+        self.assertEqual(self.sll.size, 1)
+
+    def test__getitem__raises_index_error(self):
+        with self.assertRaises(IndexError):
+            var = self.sll[0]
+        with self.assertRaises(IndexError):
+            var = self.sll[5]
 
 
-# Remember about this flashcard: Handling Expected Failures
+class TestSinglyLinkedListWithElements(unittest.TestCase):
+    def setUp(self):
+        self.sll = SinglyLinkedList()
+        for i in range(10):
+            data = 'Some Data - Node ' + str(i)
+            self.sll.add_last(data)
+
+    def test_size(self):
+        self.assertEqual(self.sll.size, 10)
+
+    def test_is_empty(self):
+        self.assertFalse(self.sll.size == 0)
+
+    def test_add_first(self):
+        self.sll.add_first("first")
+        self.assertEqual(self.sll._SinglyLinkedList__head.data, "first")
+        self.assertEqual(self.sll._SinglyLinkedList__head.next.data, "Some Data - Node 0")
+
+    def test_add_last(self):
+        self.sll.add_last("last")
+        self.assertEqual(self.sll._SinglyLinkedList__tail.data, "last")
+
+    def test_clear_size_resets_to_0(self):
+        self.sll.clear()
+        self.assertEqual(self.sll.size, 0)
+
+    def test_clear_no_elements_are_left_in_the_list(self):
+        self.sll.clear()
+        self.assertIsNone(self.sll._SinglyLinkedList__head)
+        self.assertIsNone(self.sll._SinglyLinkedList__tail)
+
+    def test__getitem__(self):
+        for i in range(self.sll.size):
+            self.assertEqual(self.sll[i], "Some Data - Node " + str(i))
+
+    def test__getitem__raises_index_error(self):
+        with self.assertRaises(IndexError):
+            var = self.sll[25]
+
+    def test_check_at_index(self):
+        for i in range(self.sll.size):
+            self.assertEqual(self.sll.check_at_index(i), "Some Data - Node " + str(i))
 
 
 if __name__ == '__main__':
