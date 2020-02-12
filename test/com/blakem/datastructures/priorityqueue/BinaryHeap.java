ad5bdef6ad3d93e6338f04bf69e337bea0d08903
blake-m <blazej.marciniak.dent@gmail.com>
2020-02-12 19:33:46 +0100

    Add Java BinaryHeap Empty Binary Heap tests

--- test/com/blakem/datastructures/priorityqueue/BinaryHeap.java
+++ test/com/blakem/datastructures/priorityqueue/BinaryHeap.java
@@ -3,7 +3,14 @@ package com.blakem.datastructures.priorityqueue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class BinaryHeapTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+
+class BinaryHeapEmptyTest {
     private BinaryHeap<Integer> bH;
 
     @BeforeEach
@@ -12,8 +19,78 @@ class BinaryHeapTest {
     }
 
     @Test
-    void initializeBinaryHeapWithSpecifiedSizeTest() {
+    void initializeBinaryHeapWithSpecifiedSize() {
+        bH = new BinaryHeap<Integer>(16);
+    }
 
+    @Test
+    void isEmptyReturnsTrueWhenEmpty() {
+        assertTrue(bH.isEmpty());
+    }
+
+    @Test
+    void sizeReturns0WhenEmpty() {
+        assertEquals(0, bH.size());
+    }
+
+    @Test
+    void peekRootReturnsNullWhenEmpty() {
+        assertNull(bH.peekRoot());
+    }
+
+    @Test
+    void pollThrowsErrorWhenEmpty() {
+        assertThrows(IndexOutOfBoundsException.class, bH::poll);
+    }
+
+    @Test
+    void addThrowsErrorWithIllegalArgument() {
+        assertThrows(IllegalArgumentException.class, () -> {
+                bH.add(null);
+            }
+        );
+    }
+
+
+
+    @Test
+    void toStringTest() {
+        // Works for elements up to 4 chars long
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int n: arr) {
+            int sol = (int)Math.pow(2, n) - 1;
+            System.out.println(sol);
         }
     }
 }
+
+class BinaryHeapWith15ElementsTest {
+    private BinaryHeap<Integer> bH;
+
+    @BeforeEach
+    void setUp() {
+        bH = new BinaryHeap<Integer>(16);
+    }
+
+    @Test
+    void initializeBinaryHeapWithSpecifiedSize() {
+        int i;
+    }
+
+    @Test
+    void toStringReturnsStringRepresentation() {
+        // Works for elements up to 4 chars long
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int n: arr) {
+            int sol = (int)Math.pow(2, n) - 1;
+            System.out.println(sol);
+        }
+    }
+}
+//_____________________nnnn_____________________
+//                    /    \
+//          ______nnnn_    _nnnn______
+//              /    \     /    \
+//            nnnn__nnnn__nnnn__nnnn
+//
+//nnnn__nnnn__nnnn__nnnn__nnnn__nnnn__nnnn__nnnn
